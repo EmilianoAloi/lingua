a1f7379afcaadae527ba56847f761b736e91efbb
Emiliano <emilianoaloi.dev@gmail.com>
2024-09-22 15:38:29 -0300

    timeline responsive

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -54,12 +54,6 @@ const Timeline = () => {
 
       <div className="">
         <div className="">
-          <div className="text-center">
-            <h2 className="text-2xl md:text-4xl text-slate-800 font-extrabold mb-4">
-              ¿Cómo es el camino?
-            </h2>
-          </div>
-
           <div className="mx-auto mt-16 md:w-3/4">
             <div className="timeline-container">
               <ul
@@ -72,46 +66,46 @@ const Timeline = () => {
                 </div>
 
                 <li className={activeItems.includes(0) ? "active" : ""}>
-                  <h4 className="text-2xl font-bold text-slate-800 mb-3">
+                  <h4 className="text-lg md:text-2xl font-bold text-slate-800 mb-3">
                     Prepará tu inscripción al examen:
                   </h4>
-                  <p className="text-slate-800 text-lg">
+                  <p className="text-slate-800 text-sm md:text-lg">
                     Regístrate con tiempo y asegúrate de cumplir con todos los
                     requisitos. Escoge la fecha del examen y completa el
                     formulario de inscripción online para reservar tu lugar.
                   </p>
-                  <div className="icon-holder">
-                    <i className="bi-search"></i>
+                  <div className="icon-holder text-white text-3xl font-bold">
+                    1
                   </div>
                 </li>
 
                 <li className={activeItems.includes(1) ? "active" : ""}>
-                  <h4 className="text-2xl font-bold text-slate-800 mb-3">
+                  <h4 className="text-lg md:text-2xl font-bold text-slate-800 mb-3">
                     Estudiá con materiales oficiales:
                   </h4>
-                  <p className="text-slate-800 text-lg">
+                  <p className="text-slate-800 text-sm md:text-lg">
                     Dedica tiempo a estudiar con los recursos proporcionados por
                     el instituto. Aprovecha las clases de preparación y
                     ejercicios prácticos diseñados para que domines todas las
                     habilidades del idioma: lectura, escritura, comprensión
                     auditiva y expresión oral.
                   </p>
-                  <div className="icon-holder">
-                    <i className="bi-bookmark"></i>
+                  <div className="icon-holder text-white text-3xl font-bold">
+                    2
                   </div>
                 </li>
 
                 <li className={activeItems.includes(2) ? "active" : ""}>
-                  <h4 className="text-2xl font-bold text-slate-800 mb-3">
+                  <h4 className="text-lg md:text-2xl font-bold text-slate-800 mb-3">
                     Presenta el examen y recibe tu certificación:
                   </h4>
-                  <p className="text-slate-800 text-lg">
+                  <p className="text-slate-800 text-sm md:text-lg">
                     Llega el día de la prueba. Mantén la calma, demuestra lo que
                     has aprendido y una vez aprobado, obtendrás un certificado
                     internacionalmente reconocido por tu nivel de inglés.
                   </p>
-                  <div className="icon-holder">
-                    <i className="bi-book"></i>
+                  <div className="icon-holder text-white text-3xl font-bold">
+                    3
                   </div>
                 </li>
               </ul>
@@ -121,7 +115,10 @@ const Timeline = () => {
           <div className="col-12 text-center mt-10">
             <p className="text-slate-800">
               ¿Querés saber mas?
-              <button href="#" className="btn text-white shadow-xl  ms-3">
+              <button
+                href="#"
+                className="btn text-white shadow-xl mt-5 md:mt-0 ms-3"
+              >
                 + Informacion sobre Examen AACI
               </button>
             </p>
